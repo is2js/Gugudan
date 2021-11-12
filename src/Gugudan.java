7f3bb57180f9b4fe02709f30c5c7b4d4bc4a89f0
is2js <tingstyle1@gmail.com>
2021-11-12 12:11:38 +0900

    변수로결과값받아보기, 사용자입력으로 바뀌는 부분을 처리해보기

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,30 +1,71 @@
+import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		// 2단 (몇단인지 알기 위해 주석 추가)
-		System.out.println("2단");
-		System.out.println(2 * 1);
-		System.out.println(2 * 2);
-		System.out.println(2 * 3);
-		System.out.println(2 * 4);
-		System.out.println(2 * 5);
-		System.out.println(2 * 6);
-		System.out.println(2 * 7);
-		System.out.println(2 * 8);
-		System.out.println(2 * 9);
-		
-		// 3단
-		System.out.println("3단");
-		System.out.println(3 * 1);
-		System.out.println(3 * 2);
-		System.out.println(3 * 3);
-		System.out.println(3 * 4);
-		System.out.println(3 * 5);
-		System.out.println(3 * 6);
-		System.out.println(3 * 7);
-		System.out.println(3 * 8);
-		System.out.println(3 * 9);
+//		1강) 무식하게 출력
+//		// 2단 (몇단인지 알기 위해 주석 추가)
+//		System.out.println("2단");
+//		System.out.println(2 * 1);
+//		System.out.println(2 * 2);
+//		System.out.println(2 * 3);
+//		System.out.println(2 * 4);
+//		System.out.println(2 * 5);
+//		System.out.println(2 * 6);
+//		System.out.println(2 * 7);
+//		System.out.println(2 * 8);
+//		System.out.println(2 * 9);
+//		
+//		// 3단
+//		System.out.println("3단");
+//		System.out.println(3 * 1);
+//		System.out.println(3 * 2);
+//		System.out.println(3 * 3);
+//		System.out.println(3 * 4);
+//		System.out.println(3 * 5);
+//		System.out.println(3 * 6);
+//		System.out.println(3 * 7);
+//		System.out.println(3 * 8);
+//		System.out.println(3 * 9);
 			
+//		2강) 1개 변수에 한번 담은 뒤 출력
+//		// 4단 (변수에 결과값을 담아 출력 구현)
+//		System.out.println("4단");
+//		int result = 4 * 1; 
+//		System.out.println(result);
+//		result = 4 * 2;
+//		System.out.println(result);
+//		result = 4 * 3;
+//		System.out.println(result);
+//		result = 4 * 4;
+//		System.out.println(result);
+//		result = 4 * 5;
+//		System.out.println(result);
+//		result = 4 * 6;
+//		System.out.println(result);
+//		result = 4 * 7;
+//		System.out.println(result);
+//		result = 4 * 8;
+//		System.out.println(result);
+//		result = 4 * 9;
+//		System.out.println(result);
+		
+		// 5단
+		// 5단부터는 몇단인지 입력받아 사용. -> [사용자입력]을 통해, 반복사용되면서 [바뀌는 부분만 따로 입력받는다]
+		// 사용자입력은 jdk제공 Scanner 클래스를 이용함. -> class 바깥 가장 상단에 추가한다.
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
+		
+		
 	}
 }
 
